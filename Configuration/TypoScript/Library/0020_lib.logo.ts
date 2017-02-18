edbb1ea71d60d9d94ec917f8b413a4bc71c15dd0
Michael Oehlhof <typo3@oehlhof.de>
2017-02-18 13:07:57 +0100

    Initial commit

---
+++ Configuration/TypoScript/Library/0020_lib.logo.ts
@@ -0,0 +1,11 @@
+# Logo in der Menue-Leiste
+lib.logo = IMAGE
+lib.logo {
+    file = EXT:typo3ruhr/Resources/Public/Icons/typo3kurs-logo.png
+    altText = Logo Meine Firma
+    stdWrap.typolink {
+        parameter = 1
+        title = Homepage TYPO3-Ruhr Training
+        ATagParams = class="navbar-brand"
+    }
+}
---
+++ Configuration/TypoScript/setup.ts
@@ -0,0 +1,4 @@
+# page = PAGE
+# page.10 = TEXT
+# page.10.value = Hurra, unsere Template-Extension funktioniert
+<INCLUDE_TYPOSCRIPT: source="DIR: ./Library" extensions="ts">
