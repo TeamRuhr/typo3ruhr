a1d217db1dfe8699c9cff60689dbc431442f14e2
Michael Oehlhof <typo3@oehlhof.de>
2017-02-18 14:48:06 +0100

    Add config for indexed_search

--- Configuration/TypoScript/Library/0010_config.ts
+++ Configuration/TypoScript/Library/0010_config.ts
@@ -1,9 +1,12 @@
-config {
-    language = de
-    locale_all = de_DE.utf8
-    sys_language_uid = 0
+page {
+	config {
+		language = de
+		locale_all = de_DE.utf8
+		sys_language_uid = 0
+		index_enable = 1
 
-    # Eindeutigere Fehlermeldungen im Frontend
-    # siehe https://docs.typo3.org/typo3cms/TyposcriptReference/Setup/Config/Index.html#contentobjectexceptionhandler
-    contentObjectExceptionHandler = 1
+		# Eindeutigere Fehlermeldungen im Frontend
+		# siehe https://docs.typo3.org/typo3cms/TyposcriptReference/Setup/Config/Index.html#contentobjectexceptionhandler
+		contentObjectExceptionHandler = 1
+	}
 }
