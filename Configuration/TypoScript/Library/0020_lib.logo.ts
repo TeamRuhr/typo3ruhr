096aeecd3b5e70f0990d4f9212984c0c2d1b0a00
Michael Oehlhof <typo3@oehlhof.de>
2017-02-18 16:08:07 +0100

    Changed logo file name and description

--- Configuration/TypoScript/Library/0020_lib.logo.ts
+++ Configuration/TypoScript/Library/0020_lib.logo.ts
@@ -1,11 +1,11 @@
 # Logo in der Menue-Leiste
 lib.logo = IMAGE
 lib.logo {
-    file = EXT:typo3ruhr/Resources/Public/Icons/typo3kurs-logo.png
-    altText = Logo Meine Firma
+    file = EXT:typo3ruhr/Resources/Public/Icons/typo3-stammtisch-rheinruhr-logo.png
+    altText = Logo TYPO3 Stammtisch RheinRuhr
     stdWrap.typolink {
         parameter = 1
-        title = Homepage TYPO3-Ruhr Training
+        title = Homepage TYPO3 Stammtisch RheinRuhr
         ATagParams = class="navbar-brand"
     }
 }
