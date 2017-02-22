d7b7949899d3102b3ad5a0df3a9a0a7774473fa0
Christian Stern <jetzt-mal-ehrlich@ich-bin-ein-arschloch.de>
2017-02-22 09:48:43 +0100

    Add an additional current state to lib.rootline to show  the news title in single view.

--- Configuration/TypoScript/Library/0060_lib.rootline.ts
+++ Configuration/TypoScript/Library/0060_lib.rootline.ts
@@ -1,24 +1,57 @@
 # Brotkrumen-Navigation
-lib.rootline = HMENU
+lib.rootline = COA
 lib.rootline {
-    special = rootline
-    special.range = 0|-1
+    10 = HMENU
+    10 {
+        special = rootline
+        special.range = 0 | -1
 
-    wrap = <ol class="breadcrumb">|</ol>
+        1 = TMENU
+        1 {
+            NO = 1
+            NO {
+                allWrap = <li>|</li>
+                stdWrap.htmlSpecialChars = 1
+                ATagTitle.field = abstract // description // subtitle // title
+            }
 
-    1 = TMENU
-    1 {
-        NO = 1
-        NO {
-            allWrap = <li>|</li>
-            stdWrap.htmlSpecialChars = 1
-            ATagTitle.field = abstract // description // subtitle // title
-        }
-
-        CUR < .NO
-        CUR {
-            allWrap = <li class="active">|</li>
-            doNotLinkIt = 1
+            CUR < .NO
+            CUR {
+                allWrap = <li class ="active">|</li>
+                doNotLinkIt = 1
+            }
         }
     }
+
+    stdWrap {
+        wrap = <ol class ="breadcrumb">|</ol>
+    }
 }
+
+[globalVar =  GP:tx_news_pi1|action = detail]
+    lib {
+        rootline {
+            10.1.CUR >
+
+            20 = COA
+            20 {
+                1 = LOAD_REGISTER
+                1 {
+                    currentNewsUid.data = GP:tx_news_pi1|news
+                    currentNewsUid.intval = 1
+                }
+
+                10 = TEXT
+                10 {
+                    data = DB:tx_news_domain_model_news:{REGISTER:currentNewsUid}:title // DB:tx_news_domain_model_news:{REGISTER:currentNewsUid}:alternative_title
+                    data.insertData = 1
+
+                    stdWrap {
+                        required = 1
+                        wrap = <li class="active">|</li>
+                    }
+                }
+            }
+        }
+    }
+[global]
