07fb9c74da2f25ec95ba859599fd3250ef79059b
Christian Stern <jetzt-mal-ehrlich@ich-bin-ein-arschloch.de>
2017-02-23 11:15:13 +0100

    Add a input field val clearer to indexed-search input.

--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -1,1 +1,16 @@
+$(document).ready( function() {
+    $('.tx-indexedsearch-searchbox-sword').on('click focusin', function () {
+        var $$ = $(this);
+        $$.attr('data-default-value', $$.attr('value'));
 
+        if ($$.val() ==  $$.attr('data-default-value')) {
+            $$.val("");
+        }
+    }).on('focusout', function () {
+        var $$ = $(this);
+
+        if ($$.val() == "") {
+            $$.val($$.attr('data-default-value'));
+        }
+    });
+});
