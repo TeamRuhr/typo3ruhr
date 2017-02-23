7a8d0686f355f541ae3528e4308a6794c4b62f03
Christian Stern <jetzt-mal-ehrlich@ich-bin-ein-arschloch.de>
2017-02-23 10:09:07 +0100

    Renamed user- and pageTS back to txt file. Otherwise they were not loaded.

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,8 +3,8 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/Page.t3s">'
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/Page.txt">'
 );
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/User.t3s">'
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/User.txt">'
 );
